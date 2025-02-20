cf95313312d9d9804e153659f4e38d35bd8eb970
Stephen Stewart <stephen.stewart@carisenda.com>
2025-02-20 12:47:53 +0000

    TF init

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///Users/sstewart/.colima/default/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
