6d05e93c6913cdf37e05814dbd25a781e15b3ac0
Stephen Stewart <stephen.stewart@carisenda.com>
2025-03-01 22:07:55 +0000

    Partial config of passkeys flow

--- main.tf
+++ main.tf
@@ -43,6 +43,121 @@ resource "keycloak_user" "main" {
   }
 }
 
+resource "keycloak_authentication_flow" "main" {
+  alias       = "browser-passkeys"
+  description = "Browser based authentication with passkeys"
+  provider_id = "basic-flow"
+  realm_id    = "playground"
+}
+
+# first execution
+resource "keycloak_authentication_execution" "execution_one" {
+  realm_id          = keycloak_realm.main.id
+  parent_flow_alias = keycloak_authentication_flow.main.alias
+  authenticator     = "auth-cookie"
+  priority          = 10
+  requirement       = "ALTERNATIVE"
+}
+
+resource "keycloak_authentication_execution" "kerberos" {
+  realm_id          = keycloak_realm.main.id
+  parent_flow_alias = keycloak_authentication_flow.main.alias
+  authenticator     = "auth-spnego"
+  priority          = 20
+}
+
+# second execution
+resource "keycloak_authentication_execution" "idp-redirect" {
+  realm_id          = keycloak_realm.main.id
+  parent_flow_alias = keycloak_authentication_flow.main.alias
+  authenticator     = "identity-provider-redirector"
+  priority          = 30
+  requirement       = "ALTERNATIVE"
+
+  depends_on = [
+    keycloak_authentication_execution.execution_one
+  ]
+}
+
+resource "keycloak_authentication_execution" "passkeys-auth" {
+  realm_id          = keycloak_realm.main.id
+  parent_flow_alias = keycloak_authentication_flow.main.alias
+  authenticator     = "passkeys-authenticator"
+  priority          = 40
+  requirement       = "ALTERNATIVE"
+}
+
+# resource "keycloak_authentication_execution" "idp-form" {
+#   realm_id          = keycloak_realm.main.id
+#   parent_flow_alias = keycloak_authentication_flow.main.alias
+#   authenticator     = "identity-provider-form"
+#   requirement       = "ALTERNATIVE"
+# 
+#   depends_on = [
+#     keycloak_authentication_execution.idp-redirect
+#   ]
+# }
+
+resource "keycloak_authentication_subflow" "organisation" {
+  realm_id          = keycloak_realm.main.id
+  alias             = "passkeys Organisation"
+  parent_flow_alias = keycloak_authentication_flow.main.alias
+  priority          = 50
+  requirement       = "ALTERNATIVE"
+}
+
+resource "keycloak_authentication_subflow" "browser-conditional-organisation" {
+  realm_id          = keycloak_realm.main.id
+  alias             = "passkeys Browser - Conditional Organisation"
+  parent_flow_alias = keycloak_authentication_subflow.organisation.alias
+  requirement       = "CONDITIONAL"
+}
+
+resource "keycloak_authentication_execution" "condition-user-configured" {
+  realm_id          = keycloak_realm.main.id
+  parent_flow_alias = keycloak_authentication_subflow.browser-conditional-organisation.alias
+  authenticator     = "conditional-user-configured"
+  priority          = 10
+  requirement       = "REQUIRED"
+}
+
+resource "keycloak_authentication_execution" "condition-organisation-configured" {
+  realm_id          = keycloak_realm.main.id
+  parent_flow_alias = keycloak_authentication_subflow.browser-conditional-organisation.alias
+  authenticator     = "organization"
+  priority          = 20
+  requirement       = "ALTERNATIVE"
+}
+
+resource "keycloak_authentication_subflow" "forms" {
+  realm_id          = keycloak_realm.main.id
+  alias             = "passkeys - Forms"
+  description       = "Username, password, OTP and other auth forms"
+  parent_flow_alias = keycloak_authentication_flow.main.alias
+  priority          = 60
+  requirement       = "ALTERNATIVE"
+}
+
+resource "keycloak_authentication_execution" "forms_username" {
+  realm_id          = keycloak_realm.main.id
+  parent_flow_alias = keycloak_authentication_subflow.forms.alias
+  authenticator     = "auth-username-password-form"
+  requirement       = "REQUIRED"
+}
+
+resource "keycloak_authentication_subflow" "browser_conditional_otp" {
+  realm_id          = keycloak_realm.main.id
+  alias             = "passkeys Browser - Conditional OTP"
+  description       = "Flow to determine if a OTP is required for the authentication"
+  parent_flow_alias = keycloak_authentication_subflow.forms.alias
+  requirement       = "CONDITIONAL"
+}
+
+resource "keycloak_authentication_bindings" "browser_authentication_binding" {
+  realm_id     = keycloak_realm.main.id
+  browser_flow = keycloak_authentication_flow.main.alias
+}
+
 data "keycloak_realm_keys" "realm_keys" {
   realm_id   = keycloak_realm.main.id
   algorithms = ["RS256"]
@@ -68,5 +183,10 @@ output "client_id" {
 output "client_secret" {
   value     = data.keycloak_openid_client.main.client_secret
   sensitive = true
+}
+
+resource "local_file" "public_key" {
+  content  = "-----BEGIN PUBLIC KEY-----\n${data.keycloak_realm_keys.realm_keys.keys[0].public_key}\n-----END PUBLIC KEY-----"
+  filename = "public.pem"
 }
 
